73c8529c140b93be6598e390a4a797c0c7ab3c21
Chinnaphat <chinnaphat46@gmail.com>
2022-03-21 17:59:34 +0700

    Modify

--- src/main/java/com/myworkshop/linebot/Application.java
+++ src/main/java/com/myworkshop/linebot/Application.java
@@ -1,25 +1,18 @@
 package com.myworkshop.linebot;
 
-import com.linecorp.bot.model.event.MessageEvent;
-import com.linecorp.bot.model.event.message.TextMessageContent;
-import com.linecorp.bot.model.message.Message;
-import com.linecorp.bot.model.message.TextMessage;
-import com.linecorp.bot.spring.boot.annotation.EventMapping;
-import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-@LineMessageHandler
-public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
-    @EventMapping
-    public Message handleTextMessage(MessageEvent<TextMessageContent> e) {
-        System.out.println("event: " + e);
-        TextMessageContent message = e.getMessage();
-        return new TextMessage(message.getText());
+@SpringBootApplication
+public class Application {
+    static Path downloadedContentDir;
+
+    public static void main(String[] args) throws IOException {
+        downloadedContentDir = Files.createTempDirectory("line-bot");
+        SpringApplication.run(Application.class, args);
     }
 }
